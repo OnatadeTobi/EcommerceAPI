23620217f655eebe7e6da131350c1a3fa6cab809
tobi <onatadetobi212@gmail.com>
2025-06-24 19:19:03 +0100

    deployment changes 4

--- ecommerceAPI/settings.py
+++ ecommerceAPI/settings.py
@@ -35,10 +35,8 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-# ALLOWED_HOSTS = ['e240-2a09-bac5-4dd5-14dc-00-214-98.ngrok-free.app', '127.0.0.1']
-# CSRF_TRUSTED_ORIGINS = ['https://e240-2a09-bac5-4dd5-14dc-00-214-98.ngrok-free.app']
-
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['e240-2a09-bac5-4dd5-14dc-00-214-98.ngrok-free.app', '127.0.0.1', 'ecommerceapi-production-e139.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://e240-2a09-bac5-4dd5-14dc-00-214-98.ngrok-free.app']
 
 
 # Application definition
