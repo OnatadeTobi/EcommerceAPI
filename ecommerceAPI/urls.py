72a96add5b032a940710619ca622b51a5155ff9c
tobi <onatadetobi212@gmail.com>
2025-06-24 23:33:03 +0100

    Added Docs with drf-yasg

--- ecommerceAPI/urls.py
+++ ecommerceAPI/urls.py
@@ -3,6 +3,21 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Ecommerce API",
+      default_version='v1',
+      description="API documentation for the Ecommerce platform",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('userauth.urls')),
@@ -15,3 +30,8 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
